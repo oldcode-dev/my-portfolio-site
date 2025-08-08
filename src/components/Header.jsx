10826c6d6d944218a31d6477d83de4e5bfd967d3
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2025-08-08 18:46:56 +0000

    Added routing and improved appearance in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import styles from './Header.module.css';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
@@ -11,19 +12,30 @@ function Header() {
     return (
         <header className={styles.header}>
             <h1 className={styles.title}>EarnestByDesign</h1>
+            <nav className={styles.largeScreenNav}>
+                <ul className={styles.navList}>
+                    <li className={styles.navItem}><Link to="/" className={styles.navLink}>Home</Link></li>
+                    <li className={styles.navItem}><Link to="/about-me" className={styles.navLink}>About Me</Link></li>
+                    <li className={styles.navItem}><a className={styles.navLink} href="#services">Skills</a></li>
+                    <li className={styles.navItem}><a className={styles.navLink} href="#portfolio">Portfolio</a></li>
+                    {/* <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#blog">Blog</a></li> */}
+                    {/* <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#testimonials">Testimonials</a></li> */}
+                    <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#contact">Contact</a></li>
+                </ul>
+            </nav>
             <button className={styles.menuButton} onClick={toggleMenu}>
                 {isMenuOpen ? '✕' : '☰'}
             </button>
             {isMenuOpen && (
                 <nav>
                     <ul className={styles.footerNavList}>
-                        <li className={styles.footerNavItem}><a className={styles.footerNavLink} href="#home">Home</a></li>
-                        <li className={styles.footerNavItem}><a className={styles.footerNavLink} href="#about">About Me</a></li>
-                        <li className={styles.footerNavItem}><a className={styles.footerNavLink} href="#services">Skills</a></li>
-                        <li className={styles.footerNavItem}><a className={styles.footerNavLink} href="#portfolio">Portfolio</a></li>
-                        {/* <li className={styles.footerNavItem}><a className={styles.footerNavLink} href="#blog">Blog</a></li> */}
-                        {/* <li className={styles.footerNavItem}><a className={styles.footerNavLink} href="#testimonials">Testimonials</a></li> */}
-                        <li className={styles.footerNavItem}><a className={styles.footerNavLink} href="#contact">Contact</a></li>
+                        <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#home">Home</a></li>
+                        <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#about">About Me</a></li>
+                        <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#services">Skills</a></li>
+                        <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#portfolio">Portfolio</a></li>
+                        {/* <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#blog">Blog</a></li> */}
+                        {/* <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#testimonials">Testimonials</a></li> */}
+                        <li className={styles.footernavItem}><a className={styles.footerNavLink} href="#contact">Contact</a></li>
                     </ul>
                 </nav>
             )}
--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,7 +1,12 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 function AboutPage(){
     return (
         <>
+            <Header/>
             <h1>About Page</h1>
+            <Footer />
         </>
     )
 }
